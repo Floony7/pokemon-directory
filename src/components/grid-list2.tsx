b3362d254df5e737a348db19c39175395a54c331
Floony7 <fred.lunjevich@stretchsense.com>
2024-04-15 11:55:23 +0100

    Layout tweaks for production

--- src/components/grid-list2.tsx
+++ src/components/grid-list2.tsx
@@ -5,10 +5,12 @@ export default async function GridList2({ query }: { query?: string }) {
   const fetchData = await fetchBySearchTerm(query ?? "");
 
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {fetchData !== undefined
-        ? fetchData?.map((p) => <PokemonCard key={p.name} pokemon={p} />)
-        : null}
+    <section className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {fetchData !== undefined ? (
+        fetchData?.map((p) => <PokemonCard key={p.name} pokemon={p} />)
+      ) : (
+        <h3>Failed to fetch data</h3>
+      )}
     </section>
   );
 }
